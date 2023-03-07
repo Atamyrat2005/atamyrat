1f0b59b62b73b3f4921202085aeb64c73b7c570a
ᗩTᗩᗰYᖇᗩT <komputeratamyrat@gmail.com>
2023-03-07 13:46:22 +0500

    1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyFirstApp());
+
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: const Text("My First App"),
+          title: const Text("My First app"),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text(
-            "This is My Homepage",
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                LinearProgressIndicator(value: 23),
+                Text(
+                  "23 %",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  "Press button to download",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
+            ),
           ),
         ),
+        floatingActionButton: const FloatingActionButton(
+            onPressed: null,
+          child: Icon(Icons.download),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
