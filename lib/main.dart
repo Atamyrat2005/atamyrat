d26117f94ca107ef71dcbd3c981d21ff5ea8ae38
ᗩTᗩᗰYᖇᗩT <komputeratamyrat@gmail.com>
2023-03-07 20:38:26 +0500

    1.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,27 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyFirstApp());
 
-class MyFirstApp extends StatefulWidget {
+class MyFirstApp extends StatelessWidget {
   const MyFirstApp({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MyFirstAppState();
-  }
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  late bool _loading;
-  late double _progressValue;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: const Text("My First app"),
-          centerTitle: true,
+          title: const Text("Adding assets"),
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      LinearProgressIndicator(value: _progressValue),
-                      Text(
-                        "${(_progressValue * 100).round()}%",
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ],
-                  )
-                : const Text(
-                    "Press button to download",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              const Image(image: AssetImage('assets/images/bg.jpg')),
+              Image.asset("assets/icons/icon.png"),
+            ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: const Icon(Icons.download),
         ),
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
-
-        if (_progressValue.toStringAsFixed(1) == '1.0'){
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
   }
 }
